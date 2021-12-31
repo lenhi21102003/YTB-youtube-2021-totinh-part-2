fd98fcdf5b3a3cf03747c2b6733ce69e535fb88c
lenhi21102003 <96907644+lenhi21102003@users.noreply.github.com>
2021-12-31 18:31:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
-    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
-    btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
-    reasonPlaceholder: 'Lý do',
-    btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
-    btnAccept: 'Ok lun 💖',
-    messLink: 'https://m.me/PhungQuocDinh'
+    introTitle: // 1. Chỉnh sửa các chữ dòng theo mục tiêu của bạn
+const  CONFIG  =  {
+    introTitle : '🖐🖐🖐 Hee loo! 🖐🖐🖐 ' ,
+    introDesc : `tao có điều này muốn hỏi mày, mày nhớ phải trả lời thật lòng nha` ,
+    btnIntro : 'Được' ,
+    title : 'mày know mày thích tao phải không? 😙 ' ,
+    desc : 'mày không trả lời hoặc thoát ra, tức là mày thích tao đó nha:>' ,
+    btn Có : 'Có, tao thật sự thích mày 💖💖💖' ,
+    btnNo : '💔 mày mơ à! 💔 ' ,
+    question : 'Nói cho tao biết đi vì sao mày thích tao 😊😊' ,
+    reasonPlaceholder : 'Lý do' ,
+    btnReply : 'Gởi! 💌💌 ' ,
+    reply : 'Tại vì tao dễ thương quá mà Ahiiiii 😊😊' ,
+    mess : 'tao biết mà, tao cũng thích mày lắm 🥰. làm ny tao nhaaaaa  😘😘 ' ,
+    messDesc : 'đồng ý r đó  ' ,
+    btnAccept : 'rồi ok  💖' , 
 }
